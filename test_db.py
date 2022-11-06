4752a94c7620e96ed835d7e595360a2410b24438
Wafiakmal <104295829+wafiakmal@users.noreply.github.com>
2022-11-06 19:13:51 +0000

    adding test file

--- test_db.py
+++ test_db.py
@@ -6,15 +6,8 @@ import os
 import sys
 import fire
 
-# Create a test database
+# test create_db
 def test_create_db():
     spotify = SpotifyDB()
     spotify.create_db()
-    assert os.path.exists('spotify.db'), "The database was not created"
-    conn = sqlite3.connect("spotify.db")
-    cnc = conn.cursor()
-    cnc.execute("SELECT * FROM spotify LIMIT 10")
-    assert len(cnc.fetchall()) == 10, "The number of rows is not correct"
-    conn.commit()
-    conn.close()
-    
+    assert os.path.exists("spotify.db")
